04539a90673d3d3c3bd043bfb46958d8f95973da
PuzyrjovMaxim <113367308+PuzyrjovMaxim@users.noreply.github.com>
2023-05-02 10:39:50 +0300

    Update form.php

--- form.php
+++ form.php
@@ -43,26 +43,26 @@ if (!empty($messages)) {
   </div>
   <div <?php if ($errors['sex']) {print 'class="error"';} ?>>
   <label>Пол: </label>
-  <span><input type="radio" name="sex" value="0">М</span>
-  <span><input type="radio" name="sex" value="1">Ж</span>
+  <span><input type="radio" name="sex" value="1" <?php if($values['sex'] == '1') {print 'checked';} ?>/> М </span>
+  <span><input type="radio" name="sex" value="2" <?php if($values['sex'] == '2') {print 'checked';} ?>/> Ж </span>
   </div>
   <p></p>
   <div <?php if ($errors['limbs']) {print 'class="error"';} ?>>
   <label>Number of limbs: </label>
-  <span><input type="radio" name="limbs" value="1">1</span>
-  <span><input type="radio" name="limbs" value="2">2</span>
-  <span><input type="radio" name="limbs" value="3">3</span>
-  <span><input type="radio" name="limbs" value="4">4</span>
-  <span><input type="radio" name="limbs" value="5">5</span>
+  <span><input type="radio" name="limbs" value="1" <?php if($values['limbs'] == '1') {print 'checked'; } ?>/> 1 </span>
+  <span><input type="radio" name="limbs" value="2" <?php if($values['limbs'] == '2') {print 'checked'; } ?>/> 2 </span>
+  <span><input type="radio" name="limbs" value="3" <?php if($values['limbs'] == '3') {print 'checked';} ?>/> 3 </span>
+  <span><input type="radio" name="limbs" value="4" <?php if($values['limbs'] == '4') {print 'checked'; } ?>/> 4 </span>
+  <span><input type="radio" name="limbs" value="5" <?php if($values['limbs'] == '5') {print 'checked'; } ?>/> 5 </span>
   </div>
   <p></p>
   <div>
-  <select name="ability[]" multiple="multiply" <?php if ($errors['ability']) {print 'class="error"';} ?> value="<?php print $values['ability']; ?>">
-    <option value="1">нет</option>
-    <option value="2">Телепотрация</option>
-    <option value="3">Невидимость</option>
-    <option value="4">Мгновенный перевод</option>
-    <option value="5">полет</option>
+  <select name="ability[]" multiple="multiply" <?php if ($errors['ability']) {print 'class="error"';} ?>>
+    <option value="1" <?php if($values['1'] == 1) {print 'selected';} ?>> нет </option>
+    <option value="2" <?php if($values['2'] == 1) {print 'selected';} ?>> Телепотрация </option>
+    <option value="3" <?php if($values['3'] == 1) {print 'selected';} ?>> Невидимость </option>
+    <option value="4" <?php if($values['4'] == 1) {print 'selected';} ?>> Мгновенный перевод </option>
+    <option value="5" <?php if($values['5'] == 1) {print 'selected';} ?>> полет </option>
   </select>
   </div>
   <p></p>
@@ -72,5 +72,21 @@ if (!empty($messages)) {
   </div>
   <input type="submit" value="ok" />
 </form>
+<?php
+    if(empty($_SESSION['login'])){
+      echo'
+      <div class="login">
+        <p> <a href="login.php">Если имеется аккаунт, то нажмите здесь</a></p>
+      </div>';
+    }
+    else{
+      echo '
+        <div class="logout">
+          <form action="index.php" method="post">
+            <input name="logout" type="submit" value="Выйти">
+          </form>
+        </div>';
+    } 
+  ?>
 </html>
 
